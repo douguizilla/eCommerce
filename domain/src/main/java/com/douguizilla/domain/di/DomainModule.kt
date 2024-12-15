63afdd6e11b1193544b5c46f8af39e20bbc2c5d8
Douglas Gomes <douglasgommez@gmail.com>
2024-12-15 01:29:27 -0300

    Initial commit

---
+++ domain/src/main/java/com/douguizilla/domain/di/DomainModule.kt
@@ -0,0 +1,7 @@
+package com.douguizilla.domain.di
+
+import org.koin.dsl.module
+
+val domainModule = module {
+
+}
---
+++ presentation/src/main/java/com/douguizilla/ecommerce/di/PresentationModule.kt
@@ -0,0 +1,7 @@
+package com.douguizilla.ecommerce.di
+
+import org.koin.dsl.module
+
+val presentationModule = module {
+
+}
